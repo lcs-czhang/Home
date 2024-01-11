2386bc9948c7d11369b366526229c222d24be27d
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-11 14:28:43 -0500

    linear

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Stored property
+    let purpleToRed = Gradient(colors: [Color.purple, Color.red])
+    // MARK: Computed property
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .resizable()
-                .scaledToFit()
-                .frame(width:100)
-            Text("Hello, world!")
+        ZStack {
+            
+            //Bottom layer
+            LinearGradient(
+                gradient: purpleToRed, startPoint: .top, endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            //Top layer
+            VStack {
+                Image(systemName: "globe")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:100)
+                Text("Hello, world!")
+            }
         }
-        .padding()
     }
 }
 
